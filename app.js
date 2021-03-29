ac4a996974e41498884f72ac7f6de2c0584efe8e
dfradejas <fradejasdaniel@gmail.com>
2021-03-29 11:48:59 +0200

    Added a manual approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Daniel!'));
+app.get('/', (req, res) => res.send('Hello Daniel! We added a manually approval'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
